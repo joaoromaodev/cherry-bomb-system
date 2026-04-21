a6bac73567bcf7cfc6a20a43c2a7aac46fd7e7bf
João Romão <romaocr33@gmail.com>
2026-04-21 19:00:47 -0300

    a

---
+++ config.js
@@ -0,0 +1,6 @@
+// config.js
+export const CONFIG = {
+  SUPABASE_URL: 'https://zrphisdhooskjrogivcs.supabase.co',
+  SUPABASE_KEY: 'sb_publishable_jhy0x1-xH5ZOOZpXssUGwQ_b3UPneKA',
+  SCHEMA: 'public'
+};
