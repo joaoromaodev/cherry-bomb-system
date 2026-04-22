0ca8fec1f21528700aea47e71f47ffcba195ad6c
João Romão <romaocr33@gmail.com>
2026-04-22 10:46:57 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // config.js — NUNCA sobe para o GitHub (.gitignore)
 // Este arquivo é usado APENAS localmente (Sandbox/Dev)
 export const CONFIG = {
-  SUPABASE_URL: 'https://zrphisdhooskjrogivcs.supabase.co',
-  SUPABASE_KEY: 'sb_publishable_jhy0x1-xH5ZOOZpXssUGwQ_b3UPneKA',
+  SUPABASE_URL: 'https://zxjeibkttmacpuukvyzo.supabase.co',
+  SUPABASE_KEY: 'sb_publishable_Vwk-3zhUoggdSkashbB9Yg_1727nPjm',
   SCHEMA: 'public'
 };
